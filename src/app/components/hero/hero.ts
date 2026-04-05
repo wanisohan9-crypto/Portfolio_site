ffdd97401048e4d655995d0dabb5d9c9fb83ba16
Sohan535 <wani.sohan.18et1108@gmail.com>
2026-04-05 21:45:30 +0530

    Fix routing for deployment

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
