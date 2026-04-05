1e92730b868a5f204458950e02af0a024bd04f37
Sohan535 <wani.sohan.18et1108@gmail.com>
2026-04-05 22:24:41 +0530

    Update API URL to Render backend

--- src/app/services/contact.ts
+++ src/app/services/contact.ts
@@ -6,12 +6,11 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ContactService {
-    private apiUrl = 'http://localhost:5000/api/contact';
+    private apiUrl = 'https://portfolio-backend-aaou.onrender.com/api/contact';
 
     constructor(private http: HttpClient) { }
 
     sendMessage(data: any): Observable<any> {
-        // return this.http.post(`${this.apiUrl}/send`, data);
         return this.http.post(this.apiUrl, data);
     }
 
